db773501265de6bd9c5cdd5644fdac40d68d6a32
argyre_j <argyre_j@1586576b-5c7f-497b-855c-82c868351710>
2015-02-06 20:02:19 +0000

    script hotel et rech index tout en php

---
+++ Decouverte de la gestion de donnees/nakach_t/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>This is a title</title>
+  </head>
+  <body>
+    
+    <?php
+       //include("script.php");
+       include("rech.php");
+       ?>
+    
+  </body>
+</html>
